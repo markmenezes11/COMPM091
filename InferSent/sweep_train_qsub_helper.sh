3872d50a757065921c78c715dc76e974984b01b9
Mark Menezes <markmenezes11@gmail.com>
2017-12-23 12:13:49 +0000

    Output for train.py on qsub fixed

--- InferSent/sweep_train_qsub_helper.sh
+++ InferSent/sweep_train_qsub_helper.sh
@@ -4,4 +4,4 @@
 #$ -l gpu=1
 #$ -P gpu
 
-singularity exec --nv --bind /cluster/project2/ishi_storage_1:/mnt /cluster/project2/ishi_storage_1/mmenezes/markmenezes11-COMPM091-master.simg python /home/mmenezes/Dev/COMPM091/InferSent/train.py $@
+singularity exec --nv --bind /cluster/project2/ishi_storage_1:/mnt /cluster/project2/ishi_storage_1/mmenezes/markmenezes11-COMPM091-master.simg python /home/mmenezes/Dev/COMPM091/InferSent/train_qsub_wrapper.py $@
