4b162c69d16b015effd6fceb6ce9a35a4c7148e3
Mark Menezes <markmenezes11@gmail.com>
2018-01-02 22:06:52 +0000

    Further fixes to SentEval-evals for InferSent

--- InferSent/SentEval-evals/infersent.py
+++ InferSent/SentEval-evals/infersent.py
@@ -28,7 +28,7 @@ class dotdict(dict):
 GLOVE_PATH = '/mnt/mmenezes/libs/InferSent/dataset/GloVe/glove.840B.300d.txt'
 PATH_SENTEVAL = '/mnt/mmenezes/libs/SentEval/'
 PATH_TO_DATA = '/mnt/mmenezes/libs/SentEval/data/senteval_data/'
-MODEL_PATH = '/mnt/mmenezes/libs/InferSent/infersent.allnli.pickle'
+MODEL_PATH = '/mnt/mmenezes/libs/InferSent/encoder/infersent.allnli.pickle'
 
 assert os.path.isfile(MODEL_PATH) and os.path.isfile(GLOVE_PATH), \
     'Set MODEL and GloVe PATHs'
