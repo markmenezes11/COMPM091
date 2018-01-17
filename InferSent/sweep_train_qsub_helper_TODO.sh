b0d07e594fe8b5b1bc08ed6a55f1adcccaab6576
Mark Menezes <markmenezes11@gmail.com>
2018-01-17 19:12:58 +0000

    GPU cheat

--- InferSent/sweep_train_qsub_helper_TODO.sh
+++ InferSent/sweep_train_qsub_helper_TODO.sh
@@ -4,6 +4,6 @@
 #$ -l gpu=1
 #$ -P gpu
 
-singularity exec --nv --bind /cluster/project2/ishi_storage_1:/mnt /cluster/project2/ishi_storage_1/mmenezes/markmenezes11-COMPM091-master.simg python /home/mmenezes/Dev/COMPM091/InferSent/train_qsub_wrapper.py $@
+singularity exec --nv --bind /cluster/project2/ishi_storage_1:/mnt /cluster/project2/ishi_storage_1/mmenezes/markmenezes11-COMPM091-master.simg python /home/mmenezes/Dev/COMPM091/InferSent/train_qsub_wrapper.py $@ --gpu_id nvidia-smi dmon -c 1 | grep -v "#" | awk '{ print $4 " " $1 }' | sort -n | awk '{ print $2 }' | tr -d "\n" | head -c 1
 
 # TODO: Set GPU ID correctly based on CUDA_VISIBLE_DEVICES (even though it seems to be broken...)
