36609242aefbf5897a1119783db77bb39f3d7e98
Mark Menezes <markmenezes11@gmail.com>
2018-01-29 15:15:43 +0000

    Added debug info to qsub scripts

--- InferSent-sweep/snli_eval_qsub_helper.sh
+++ InferSent-sweep/snli_eval_qsub_helper.sh
@@ -5,6 +5,8 @@
 #$ -P gpu
 
 hostname
+echo $CUDA_VISIBLE_DEVICES
+nvidia-smi
 $@ --gpu_id $(nvidia-smi dmon -c 1 | grep -v "#" | awk '{ print $4 " " $1 }' | sort -n | awk '{ print $2 }' | tr -d "\n" | head -c 1)
 
 # TODO: Set GPU ID correctly based on CUDA_VISIBLE_DEVICES (even though it seems to be broken...)
