9b4de73731959c4163422505c28747d2c078d5a3
Mark Menezes <markmenezes11@gmail.com>
2018-02-10 01:21:00 +0000

    Make temp folder correctly

--- SentEval-modified/senteval/binary.py
+++ SentEval-modified/senteval/binary.py
@@ -49,6 +49,8 @@ class BinaryClassifierEval(object):
             # EDITED: Save embeddings to temp pickle file and store the filename and array index for future reference
             embeddings = batcher(params, batch) # (len(batch), embedding_length), i.e. (params_senteval.batch_size, embedding_length)
             filename = "temp/batch_" + str(batch_number) + ".pkl"
+            if not os.path.exists("temp/"):
+                os.makedirs("temp")
             with open(filename, "w") as f:
                 pickle.dump(embeddings, f)
             index = 0
