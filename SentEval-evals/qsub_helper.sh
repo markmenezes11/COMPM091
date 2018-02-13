7b354479e3c9cb702023ff0d8d7544e1b59ea19c
Mark Menezes <markmenezes11@gmail.com>
2018-02-13 19:13:04 +0000

    qsub helpers

---
+++ SentEval-evals/qsub_helper.sh
@@ -0,0 +1,11 @@
+#$ -S /bin/bash
+#$ -l h_rt=23:30:0 # Time (h:m:s)
+#$ -l tmem=15.5G # Mem (xG or xM)
+#$ -l gpu=1
+#$ -P gpu
+
+hostname
+nvidia-smi
+echo CUDA_VISIBLE_DEVICES: \"$CUDA_VISIBLE_DEVICES\"
+CUDA_VISIBLE_DEVICES=$(nvidia-smi dmon -c 1 | grep -v "#" | awk '{ print $4 " " $1 }' | sort -n | awk '{ print $2 }' | tr -d "\n" | head -c 1) $@
+
