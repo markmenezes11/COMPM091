98152b61b411de9eb0be49aae729ce73202da162
Mark Menezes <markmenezes11@gmail.com>
2018-02-26 10:28:05 +0000

    Fixed bcn results parser

--- Utils/bcn_results_parser.py
+++ Utils/bcn_results_parser.py
@@ -9,17 +9,17 @@ params, _ = parser.parse_known_args()
 def parse_results(dir):
     outputslash = "" if dir[-1] == "/" else "/"
     infopath = dir + outputslash + "info.txt"
-    accuracypath = dir + outputslash + "accurac.txt"
+    accuracypath = dir + outputslash + "accuracy.txt"
     if os.path.exists(infopath) and os.path.exists(accuracypath):
         print("\nResults for: " + dir)
         with open(infopath, "r") as outputfile:
             lines = outputfile.readlines()
-            print("Sentence embeddings: " + lines[0])
-            print("Transfer task: " + lines[1])
-            #print("Hyperparameters: " + lines[2])
+            print("Sentence embeddings: " + lines[0].replace('\n', '').replace('\r', ''))
+            print("Transfer task: " + lines[1].replace('\n', '').replace('\r', ''))
+            #print("Hyperparameters: " + lines[2].replace('\n', '').replace('\r', ''))
         with open(accuracypath, "r") as outputfile:
             lines = outputfile.readlines()
-            print("Accuracy: " + lines[0])
+            print("Accuracy: " + lines[0].replace('\n', '').replace('\r', ''))
 
 for subdir, dirs, files in os.walk(params.rootdir):
     parse_results(subdir)
